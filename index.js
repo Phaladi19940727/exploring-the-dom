0f6420e2f5a5dfdba9fd12a05e03165eb462a91e
Phaladi19940727 <phaladistephina@gmail.com>
2023-06-28 11:28:31 +0200

    First commit

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ theMessageButton.addEventListener('click', function() {
 const fruitList = document.querySelector(".fruits");
 
 for(let i=0;i<fruits.length;i++){
-   // get fruits from the list
+   
    const fruit = fruits[i];
    
-   // create a new li element
+   
    const li = document.createElement('li');
    li.innerText = fruit;
    fruitList.appendChild(li);
